79b4fc489b6fd652c465bcc8ce92166fdba6fb9f
Gabriel <gs4334904@gmail.com>
2024-12-09 16:08:30 -0300

    asda

--- resources/views/admin/selecao/edit.blade.php
+++ resources/views/admin/selecao/edit.blade.php
@@ -12,9 +12,9 @@
                         <!-- Campo de Título -->
                         <div>
                             <label for="titulo" class="block text-sm font-medium text-gray-700 dark:text-white">Título</label>
-                            <input type="text" id="titulo" name="titulo" 
-                                class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" 
-                                placeholder="Título da Seleção" 
+                            <input type="text" id="titulo" name="titulo"
+                                class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                                placeholder="Título da Seleção"
                                 value="{{ $selecao->titulo ?? old('titulo') }}">
                         </div>
 
@@ -25,12 +25,12 @@
                                 <div class="mt-1">
                                     <!-- Exibir edital atual, se disponível -->
                                     @if (!empty($selecao->edital))
-                                        <a href="{{ asset($selecao->edital) }}" target="_blank" class="text-indigo-600 hover:underline">
-                                            Ver edital atual
-                                        </a>
+                                    <a href="{{ asset($selecao->edital) }}" target="_blank" class="text-indigo-600 hover:underline">
+                                        ver edital atual
+                                    </a>
                                     @endif
                                     <!-- Upload de novo edital -->
-                                    <input type="file" id="edital" name="edital" 
+                                    <input type="file" id="edital" name="edital"
                                         class="mt-2 block w-full text-sm text-gray-700 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 
                                                 file:bg-gray-50 file:border file:border-gray-300 file:rounded-md file:px-4 file:py-2 file:text-sm file:font-medium 
                                                 hover:file:bg-indigo-100 focus:file:bg-indigo-200">
@@ -39,8 +39,8 @@
 
                             <div>
                                 <label for="informacoes_gerais" class="block text-sm font-medium text-gray-700 dark:text-white">Informações Gerais</label>
-                                <input type="text" id="informacoes_gerais" name="informacoes_gerais" 
-                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" 
+                                <input type="text" id="informacoes_gerais" name="informacoes_gerais"
+                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                                     value="{{ $selecao->informacoes_gerais ?? old('informacoes_gerais') }}">
                             </div>
                         </div>
@@ -49,15 +49,15 @@
                         <div class="grid grid-cols-2 gap-4">
                             <div>
                                 <label for="inscricao_inicio" class="block text-sm font-medium text-gray-700 dark:text-white">Início da Inscrição</label>
-                                <input type="date" id="inscricao_inicio" name="inscricao_inicio" 
-                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" 
+                                <input type="date" id="inscricao_inicio" name="inscricao_inicio"
+                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                                     value="{{ $selecao->inscricao_inicio ?? old('inscricao_inicio') }}">
                             </div>
 
                             <div>
                                 <label for="inscricao_fim" class="block text-sm font-medium text-gray-700 dark:text-white">Fim da Inscrição</label>
-                                <input type="date" id="inscricao_fim" name="inscricao_fim" 
-                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" 
+                                <input type="date" id="inscricao_fim" name="inscricao_fim"
+                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                                     value="{{ $selecao->inscricao_fim ?? old('inscricao_fim') }}">
                             </div>
                         </div>
@@ -65,7 +65,7 @@
                         <!-- Outros Campos -->
                         <div class="mb-4">
                             <label for="exibir_edital" class="block text-sm font-medium text-gray-700 dark:text-white">Exibir Edital?</label>
-                            <select id="exibir_edital" name="exibir_edital" 
+                            <select id="exibir_edital" name="exibir_edital"
                                 class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
                                 <option value="1" @if (( $selecao->exibir_edital ?? old('exibir_edital')) == 1) selected @endif>Sim</option>
                                 <option value="0" @if (( $selecao->exibir_edital ?? old('exibir_edital')) == 0) selected @endif>Não</option>
@@ -74,7 +74,7 @@
 
                         <div class="mb-4">
                             <label for="exibir_resultado_inscricao" class="block text-sm font-medium text-gray-700 dark:text-white">Exibir Resultado da Inscrição?</label>
-                            <select id="exibir_resultado_inscricao" name="exibir_resultado_inscricao" 
+                            <select id="exibir_resultado_inscricao" name="exibir_resultado_inscricao"
                                 class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
                                 <option value="1" @if (( $selecao->exibir_resultado_inscricao ?? old('exibir_resultado_inscricao')) == 1) selected @endif>Sim</option>
                                 <option value="0" @if (( $selecao->exibir_resultado_inscricao ?? old('exibir_resultado_inscricao')) == 0) selected @endif>Não</option>
@@ -83,7 +83,7 @@
 
                         <div class="mb-4">
                             <label for="finalizado" class="block text-sm font-medium text-gray-700 dark:text-white">Finalizado?</label>
-                            <select id="finalizado" name="finalizado" 
+                            <select id="finalizado" name="finalizado"
                                 class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
                                 <option value="1" @if (( $selecao->finalizado ?? old('finalizado')) == 1) selected @endif>Sim</option>
                                 <option value="0" @if (( $selecao->finalizado ?? old('finalizado')) == 0) selected @endif>Não</option>
@@ -92,7 +92,7 @@
 
                         <div class="mb-4">
                             <label for="resultado" class="block text-sm font-medium text-gray-700 dark:text-white">Resultado</label>
-                            <textarea id="resultado" name="resultado" rows="4" 
+                            <textarea id="resultado" name="resultado" rows="4"
                                 class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">{{ $selecao->resultado ?? old('resultado') }}</textarea>
                         </div>
 
