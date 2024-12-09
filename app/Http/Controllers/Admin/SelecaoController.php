77ff45453dbb5b57eeb529f3a61b3c91099fa6e2
Gabriel <gs4334904@gmail.com>
2024-12-09 13:20:38 -0300

    Adicionando a função de update da seleção.

--- app/Http/Controllers/Admin/SelecaoController.php
+++ app/Http/Controllers/Admin/SelecaoController.php
@@ -44,7 +44,7 @@ class SelecaoController extends Controller
 
 
 
-       // Instanciando um novo registro para a tabela selecoes
+        // Instanciando um novo registro para a tabela selecoes
         $selecao = new Selecao();
         $selecao->titulo = $validacao['titulo'];
         $selecao->informacoes_gerais = $validacao['informacoes_gerais'];
@@ -73,6 +73,54 @@ class SelecaoController extends Controller
 
         // Retornando a resposta de sucesso
         return redirect()->route('selecao')->with('success', 'Seleção cadastrada com sucesso!');
+    }
+
+    public function update(Request $request, $id) 
+    {
+        // Localizar a seleção pelo ID
+        $selecao = Selecao::findOrFail($id);
+
+
+        // Validar os dados recebidos
+        $validacao = $request->validate([
+            'titulo' => 'required|string|max:255',
+            'edital' => 'nullable|file|mimes:pdf|max:2048',
+            'informacoes_gerais' => 'required|string',
+            'inscricao_inicio' => 'required|date',
+            'inscricao_fim' => 'required|date|after_or_equal:inscricao_inicio',
+            'exibir_edital' => 'required|boolean',
+            'exibir_resultado_inscricao' => 'required|boolean',
+            'finalizado' => 'required|boolean',
+            'resultado' => 'nullable|string',
+        ]);
+
+        // Atualizar os campos do modelo
+        $selecao->titulo = $validacao['titulo'];
+        $selecao->informacoes_gerais = $validacao['informacoes_gerais'];
+        $selecao->inscricao_inicio = $validacao['inscricao_inicio'];
+        $selecao->inscricao_fim = $validacao['inscricao_fim'];
+        $selecao->exibir_edital = $validacao['exibir_edital'];
+        $selecao->exibir_resultado_inscricao = $validacao['exibir_resultado_inscricao'];
+        $selecao->finalizado = $validacao['finalizado'];
+        $selecao->resultado = $validacao['resultado'];
+
+        // Verificar se um novo arquivo foi enviado
+        if ($request->hasFile('edital')) {
+            $file = $request->file('edital');
+            $destinationPath = 'documents';
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $path = $destinationPath . '/' . $fileName;
+
+            // Mover o arquivo para a pasta e atualizar o campo de edital
+            $file->move($destinationPath, $fileName);
+            $selecao->edital = $path;
+        }
+
+        // Salvar as alterações
+        $selecao->save();
+
+        // Retornar uma mensagem de sucesso
+        return redirect()->route('selecao')->with('success', 'Seleção atualizada com sucesso!');
     }
 
     public function destroy($id)
