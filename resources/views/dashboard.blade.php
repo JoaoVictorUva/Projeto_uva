d56ea0d1bee64c42db6d9f5bcdaaa1fc7e05a1c8
Gabriel <gs4334904@gmail.com>
2024-12-11 10:06:06 -0300

    Adicionando rotas aos botões

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,25 +11,25 @@
                 <div class="metric-value text-green-500 text-[50px] leading-[0.8] md:text-[40px]">{{$candidatos}}</div>
                 <div class="metric-title text-[30px] leading-none mt-3">Candidatos</div>
                 <div class="metric-description text-[20px] font-normal mt-3">Total de candidatos cadastrados.</div>
-                <button class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Candidatos</button>
+                <button onclick="window.location.href='{{ route('candidato') }}'" class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Candidatos</button>
             </div>
             <div class="metric-card bg-white dark:bg-gray-800 rounded-3xl shadow-2xl flex flex-col justify-start flex-1 min-w-[240px] min-h-[250px] p-8 border border-gray-200">
                 <div class="metric-value text-green-500 text-[50px] leading-[0.8] md:text-[40px]">{{$inscricoes}}</div>
                 <div class="metric-title text-[30px] leading-none mt-3">Inscrições</div>
-                <div class="metric-description text-[20px] font-normal mt-3">Total de inscrições realizadas</div>
-                <button class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Inscrições</button>
+                <div class="metric-description text-[20px] font-normal mt-3">Total de inscrições realizadas.</div>
+                <button onclick="window.location.href='{{ route('selecao') }}'" class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Inscrições</button>
             </div>
             <div class="metric-card bg-white dark:bg-gray-800 rounded-3xl shadow-2xl flex flex-col justify-start flex-1 min-w-[240px] min-h-[250px] p-8 border border-gray-200">
                 <div class="metric-value text-green-500 text-[50px] leading-[0.8] md:text-[40px]">{{$vagas}}</div>
                 <div class="metric-title text-[30px] leading-none mt-3">Vagas</div>
-                <div class="metric-description text-[20px] font-normal mt-3">Total de vagas cadastradas</div>
-                <button class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Vagas</button>
+                <div class="metric-description text-[20px] font-normal mt-3">Total de vagas cadastradas.</div>
+                <button onclick="window.location.href='{{ route('vaga') }}'" class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Vagas</button>
             </div>
             <div class="metric-card bg-white dark:bg-gray-800 rounded-3xl shadow-2xl flex flex-col justify-start flex-1 min-w-[240px] min-h-[250px] p-8 border border-gray-200">
                 <div class="metric-value text-green-500 text-[50px] leading-[0.8] md:text-[40px]">{{$selecoes}}</div>
                 <div class="metric-title text-[30px] leading-none mt-3">Seleções</div>
-                <div class="metric-description text-[20px] font-normal mt-3">Total de seleções realizadas</div>
-                <button class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Seleções</button>
+                <div class="metric-description text-[20px] font-normal mt-3">Total de seleções realizadas.</div>
+                <button onclick="window.location.href='{{ route('selecao') }}'" class="border border-green-500 bg-green-500 text-white py-2 px-4 rounded m-1 mt-4 w-full">Seleções</button>
             </div>
         </div>
     </div>
